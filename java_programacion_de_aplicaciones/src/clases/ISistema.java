77e7739cdffd860bf777c692f4572f0c16cdbf1f
Valentín Veintemilla <valentinveintemilla@gmail.com>
2024-08-20 15:50:23 -0300

    Avances en ISistema y Sistema

--- java_programacion_de_aplicaciones/src/clases/ISistema.java
+++ java_programacion_de_aplicaciones/src/clases/ISistema.java
@@ -10,35 +10,37 @@ public abstract class ISistema{
 	
 	// Le puse abstract a todo pero no se si esta bien
 	
-	public abstract bool AltaUsuarioCliente(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String imagen);
+	public abstract boolean AltaUsuarioCliente(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String imagen);
 	
-	public abstract bool AltaUsuarioProveedor(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String nomCompania, String linkWeb, String imagen);
+	public abstract boolean AltaUsuarioProveedor(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String nomCompania, String linkWeb, String imagen);
 
-	public abstract bool RegistrarProducto(String titulo, String numReferencia, String descrip, String especificaciones, int precio, Proveedor proveedor);
+	public abstract boolean RegistrarProducto(String titulo, String numReferencia, String descrip, String especificaciones, int precio, Proveedor proveedor);
 	
 	public abstract DTProductoDetallado verInformaciónProducto(int numReferencia);
 
 	public abstract List<DTCategoria> listarCategorias();
 
-	public abstract bool elegirCategoria(String nombreCat);
+	public abstract boolean elegirCategoria(String nombreCat);
 
 	public abstract List<DTProducto> listarProductos();
+	
+	public abstract boolean elegirProducto(String nombreProd); // ESTA LA AGREGUÉ DESPUÉS
 
-	public abstract void altaCategoria(String nombre, bool tieneProductos, Categoría padre);
+	public abstract void altaCategoria(String nombre, boolean tieneProductos, Categoria padre);
 
 	public abstract List<DTOrdenDeCompra> listarOrdenesDeCompra();
 
-	public abstract bool elegirOrdenDeCompra(int numero);
+	public abstract boolean elegirOrdenDeCompra(int numero);
 
-	public abstract DTOrdenCompraDetallada darAltaOrden();
+	public abstract DTOrdenDeCompraDetallada darAltaOrden();
 
 	public abstract void cancelarOrdenDeCompra(int numero);
 
-	public abstract bool agregarProducto(String nombreProducto, int cantidad);
+	public abstract boolean agregarProducto(String nombreProducto, int cantidad);
 
 	public abstract List<DTCliente> listarClientes();
 
-	public abstract bool elegirCliente(String email);
+	public abstract boolean elegirCliente(String email);
 
 	public abstract void quitarProductoDeCategorias();
 }
--- java_programacion_de_aplicaciones/src/clases/Sistema.java
+++ java_programacion_de_aplicaciones/src/clases/Sistema.java
@@ -25,7 +25,7 @@ public class Sistema extends ISistema {
 	}
 	
 	@Override // NO ES NECESARIO QUE SEA BOOL
-	public bool AltaUsuarioCliente(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String imagen) {
+	public boolean AltaUsuarioCliente(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String imagen) {
 		for (Usuario user : this.usuarios) {
 			if (user.getEmail() == email) {
 				throw new Exception("Error: Ya existe un cliente registrado con el email " + '"' + email + '"' + '.');
@@ -40,7 +40,7 @@ public class Sistema extends ISistema {
 	}
 	
 	@Override // NO ES NECESARIO QUE SEA BOOL
-	public bool AltaUsuarioProveedor(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String nomCompania, String linkWeb, String imagen) {
+	public boolean AltaUsuarioProveedor(String nickname, String email, String nombre, String apellido, DTFecha fechaNac, String nomCompania, String linkWeb, String imagen) {
 		for (Usuario user : this.usuarios) {
 			if (user.getEmail() == email) {
 				throw new Exception("Error: Ya existe un proveedor registrado con el email " + '"' + email + '"' + '.');
@@ -55,7 +55,7 @@ public class Sistema extends ISistema {
 	}
 	
 	@Override
-	public bool RegistrarProducto(String titulo, String numReferencia, String descrip, String especificaciones, int precio, Proveedor proveedor) {
+	public boolean RegistrarProducto(String titulo, String numReferencia, String descrip, String especificaciones, int precio, Proveedor proveedor) {
 		
 		// NO SUPE COMO HACERLA
 		
@@ -63,8 +63,8 @@ public class Sistema extends ISistema {
 	
 	@Override // Lo podria leer directamente
 	public DTProductoDetallado verInformaciónProducto(int numReferencia) {
-		if (this.productoActual == NULL) {
-			throw new Exception("Error: No se ha elegido un producto previamente.");
+		if (this.productoActual == null) {
+			throw new NullPointerException("Error: No se ha elegido un producto previamente.");
 		}
 		return this.productoActual.getDTProductoDetallado();
 	}
@@ -93,7 +93,7 @@ public class Sistema extends ISistema {
 	*/
 
 	@Override
-	public bool elegirCategoria(String nombreCat) { // No creo que sea necesario que sean bool (todos los elegir)
+	public boolean elegirCategoria(String nombreCat) { // No creo que sea necesario que sean bool (todos los elegir)
 		for (Categoria cat : this.categorias) {
 			if (cat.getNombre() == nombreCat) {
 				this.categoriaActual = cat;
@@ -106,9 +106,9 @@ public class Sistema extends ISistema {
 	@Override
 	public List<DTProducto> listarProductos(){
 		if (this.categoriaActual == null) {
-			throw new Exception("Error: No se ha elegido una categoría previamente.");
+			throw new NullPointerException("Error: No se ha elegido una categoría previamente.");
 		}
-		List<DTCategoria> lista = new ArrayList<>();
+		List<DTProducto> lista = new ArrayList<>();
 		for (Producto prod : this.categoriaActual.getProductos()) {
 			DTProducto dt = prod.getDTProducto();
 			lista.add(dt);
@@ -131,9 +131,9 @@ public class Sistema extends ISistema {
 
 	// No se si incluir el numero de referencia (para buscar el producto)
 	@Override
-	public bool elegirProducto(String nombreProd) {
+	public boolean elegirProducto(String nombreProd) {
 		if (this.categoriaActual == null) {
-			throw new Exception("Error: No se ha elegido una categoría previamente.");
+			throw new NullPointerException("Error: No se ha elegido una categoría previamente.");
 		}
 		for (Producto prod : this.categoriaActual.getProductos()) {
 			if (prod.getNombre() == nombreProd) {
@@ -145,7 +145,7 @@ public class Sistema extends ISistema {
 	}
 	
 	@Override
-	public void altaCategoria(String nombre, bool tieneProductos, Categoria padre) {
+	public void altaCategoria(String nombre, boolean tieneProductos, Categoria padre) {
 		for (Categoria cat : this.categorias) {
 			if (cat.getNombre() == nombre) {
 				throw new Exception("Error: Ya existe una categoría con el nombre " + '"' + nombre + '"' + '.');
@@ -157,9 +157,9 @@ public class Sistema extends ISistema {
 
 	@Override
 	public List<DTOrdenDeCompra> listarOrdenesDeCompra(){
-		List<DTCategoria> lista = new ArrayList<>();
+		List<DTOrdenDeCompra> lista = new ArrayList<>();
 		for (OrdenDeCompra ord : this.ordenes) {
-			DTOrdenDeCompra dt = prod.getDTOrden(); // Capaz la función no se llama así
+			DTOrdenDeCompra dt = ord.getDTOrden(); // Capaz la función no se llama así
 			lista.add(dt);
 		}
 		return lista;
@@ -179,27 +179,27 @@ public class Sistema extends ISistema {
 	*/
 
 	@Override
-	public bool elegirOrdenDeCompra(int numero) {
+	public boolean elegirOrdenDeCompra(int numero) {
 		for (OrdenDeCompra ord : this.ordenes) {
 			if (ord.getNumero() == numero) {
 				this.ordenActual = ord;
 				return true; // No es necesario que sea bool
 			}
 		}
-		throw new Exception("Error: La orden de compra número " + '"' + numero + '"' + " no existe.");
+		throw new IllegalArgumentException("Error: La orden de compra número " + '"' + numero + '"' + " no existe.");
 	}
 	
 	@Override
 	public void verInformacionOrdenDeCompra(int numero) {
 		if (this.ordenActual == null) {
-			throw new Exception("Error: No se ha elegido una orden de compra previamente.");
+			throw new NullPointerException("Error: No se ha elegido una orden de compra previamente.");
 		}
 		DTOrdenDeCompra dt = this.ordenActual.getDTOrden(); // Capaz la función no se llama así
 		System.out.println("---ORDEN DE COMPRA---" + "\n" + dt.toString()); // TENEMOS QUE SOBREESCRIBIR EL METODO toString de la clase DTOrdenDeCompra
 	}
 
 	@Override
-	public DTOrdenCompraDetallada darAltaOrden() {
+	public DTOrdenDeCompraDetallada darAltaOrden() {
 		
 		// NO SUPE COMO HACERLA
 		
@@ -211,11 +211,11 @@ public class Sistema extends ISistema {
 			if (ord.getNumero() == numero) {
 				this.ordenes.remove(ord);
 				ord.desvincularCliente();
-				ord.setCantidades = null;
+				ord.setCantidades(null);
 				return;
 			}
 		}
-		throw new Exception("Error: La orden de compra número " + '"' + numero + '"' + " no existe.");
+		throw new IllegalArgumentException("Error: La orden de compra número " + '"' + numero + '"' + " no existe.");
 	}
 
 	@Override
@@ -259,27 +259,27 @@ public class Sistema extends ISistema {
 					this.usuarioActual = user;
 					return true; // No es necesario que sea booleano
 				}
-				throw new Exception("Error: El usuario de email " + '"' + email + '"' + " existe, pero no es un cliente.");
+				throw new IllegalArgumentException("Error: El usuario de email " + '"' + email + '"' + " existe, pero no es un cliente.");
 			}
 		}
-		throw new Exception("Error: El usuario de email " + '"' + email + '"' + " no existe.");
+		throw new IllegalArgumentException("Error: El usuario de email " + '"' + email + '"' + " no existe.");
 	}
 
 	@Override
 	public void quitarProductoDeCategorias() {
-		if (this.productoActual == NULL) {
-			throw new Exception("Error: No se ha elegido un producto previamente.");
+		if (this.productoActual == null) {
+			throw new NullPointerException("Error: No se ha elegido un producto previamente.");
 		}
 		for(Categoria cat : this.categorias) {
-			cat.remove(this.productoActual);
+			cat.getProductos().remove(this.productoActual);
 		}
 	}
 	
 	// Funciones que no estaban
 	
 	public void agregarProductoACategorias(List<Categoria> listaCat) {
-		if (this.productoActual == NULL) {
-			throw new Exception("Error: No se ha elegido un producto previamente.");
+		if (this.productoActual == null) {
+			throw new NullPointerException("Error: No se ha elegido un producto previamente.");
 		}
 		for (Categoria cat : listaCat) {
 			cat.add(this.productoActual);
@@ -287,15 +287,15 @@ public class Sistema extends ISistema {
 	}
 	
 	public void agregarCategorias(List<Categoria> listaCat) {
-		if (this.productoActual == NULL) {
-			throw new Exception("Error: No se ha elegido un producto previamente.");
+		if (this.productoActual == null) {
+			throw new NullPointerException("Error: No se ha elegido un producto previamente.");
 		}
 		this.productoActual.setCategorias(listaCat);
 	}
 	
 	public void modificarDatosProducto(String nombreProd, int numReferencia, String descripcion, float precio, String especificacion) {
 		if (precio <= 0) {
-			throw new Exception("Error: El precio elegido no puede ser menor o igual a 0.");
+			throw new IllegalArgumentException("Error: El precio elegido no puede ser menor o igual a 0.");
 		}
 		// FALTA SEGUIR
 	}
