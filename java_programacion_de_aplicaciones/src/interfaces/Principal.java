9b97c86bc0ab92de16bc662886d6ee6b75643fb6
kingstolas12 <nadiagorrias@gmail.com>
2024-08-19 23:37:19 -0300

    registrar usuario

--- java_programacion_de_aplicaciones/src/interfaces/Principal.java
+++ java_programacion_de_aplicaciones/src/interfaces/Principal.java
@@ -11,11 +11,32 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class Principal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_4;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_5;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_14;
+	private JTextField textField_15;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
 
 	/**
 	 * Launch the application.
@@ -103,6 +124,219 @@ public class Principal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JInternalFrame RegistrarCliente = new JInternalFrame("Registrar Usuario");
+		RegistrarCliente.setBackground(new Color(253, 238, 247));
+		RegistrarCliente.setClosable(true);
+		RegistrarCliente.setBounds(0, 0, 385, 399);
+		contentPane.add(RegistrarCliente);
+		RegistrarCliente.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(106, 51, 222, 20);
+		RegistrarCliente.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Registrar Cliente");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setBounds(0, 11, 369, 29);
+		RegistrarCliente.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nickname");
+		lblNewLabel_1.setBounds(38, 54, 48, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Correo Electrónico");
+		lblNewLabel_1_2.setBounds(38, 98, 89, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_2);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(148, 95, 180, 20);
+		RegistrarCliente.getContentPane().add(textField_4);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Nombre");
+		lblNewLabel_1_1.setBounds(38, 143, 48, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(96, 140, 232, 20);
+		RegistrarCliente.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Apellido");
+		lblNewLabel_1_1_1.setBounds(38, 188, 48, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_1_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(96, 185, 232, 20);
+		RegistrarCliente.getContentPane().add(textField_2);
+		
+		JLabel lblNewLabel_1_2_1 = new JLabel("Fecha de Nacimiento");
+		lblNewLabel_1_2_1.setBounds(38, 233, 102, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_2_1);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(38, 258, 37, 20);
+		RegistrarCliente.getContentPane().add(textField_3);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("/");
+		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1_1_1_1.setBounds(85, 260, 5, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_1_1_1);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(162, 258, 37, 20);
+		RegistrarCliente.getContentPane().add(textField_6);
+		
+		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("/");
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1_1_1_1_1.setBounds(147, 260, 5, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_1_1_1_1);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(100, 258, 37, 20);
+		RegistrarCliente.getContentPane().add(textField_7);
+		
+		JLabel lblNewLabel_1_1_1_2 = new JLabel("Día");
+		lblNewLabel_1_1_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1_2.setBounds(38, 281, 37, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_1_1_2);
+		
+		JLabel lblNewLabel_1_1_1_2_1 = new JLabel("Mes");
+		lblNewLabel_1_1_1_2_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1_2_1.setBounds(100, 281, 37, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_1_1_2_1);
+		
+		JLabel lblNewLabel_1_1_1_2_1_1 = new JLabel("Año");
+		lblNewLabel_1_1_1_2_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1_2_1_1.setBounds(162, 281, 37, 14);
+		RegistrarCliente.getContentPane().add(lblNewLabel_1_1_1_2_1_1);
+		
+		JButton lblNewLabel_2 = new JButton("Asignar una imagen");
+		lblNewLabel_2.setBounds(38, 319, 144, 20);
+		RegistrarCliente.getContentPane().add(lblNewLabel_2);
+		
+		JInternalFrame RegistrarProveedor = new JInternalFrame("Registrar Usuario");
+		RegistrarProveedor.getContentPane().setBackground(new Color(253, 238, 247));
+		RegistrarProveedor.setBounds(307, 29, 385, 486);
+		contentPane.add(RegistrarProveedor);
+		RegistrarProveedor.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("Nickname");
+		lblNewLabel_1_3.setBounds(38, 54, 48, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_3);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(106, 51, 222, 20);
+		RegistrarProveedor.getContentPane().add(textField_5);
+		
+		JLabel lblNewLabel_1_2_2 = new JLabel("Correo Electrónico");
+		lblNewLabel_1_2_2.setBounds(38, 98, 89, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_2_2);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(148, 95, 180, 20);
+		RegistrarProveedor.getContentPane().add(textField_8);
+		
+		JLabel lblNewLabel_1_1_2 = new JLabel("Nombre");
+		lblNewLabel_1_1_2.setBounds(38, 143, 48, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_2);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(96, 140, 232, 20);
+		RegistrarProveedor.getContentPane().add(textField_9);
+		
+		JLabel lblNewLabel_1_1_1_3 = new JLabel("Apellido");
+		lblNewLabel_1_1_1_3.setBounds(38, 188, 48, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_1_3);
+		
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		textField_10.setBounds(96, 185, 232, 20);
+		RegistrarProveedor.getContentPane().add(textField_10);
+		
+		JLabel lblRegistrarProovedor = new JLabel("Registrar Proveedor");
+		lblRegistrarProovedor.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRegistrarProovedor.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblRegistrarProovedor.setBounds(0, 11, 369, 29);
+		RegistrarProveedor.getContentPane().add(lblRegistrarProovedor);
+		
+		JLabel lblNewLabel_1_1_2_1 = new JLabel("Nombre de la Compañía");
+		lblNewLabel_1_1_2_1.setBounds(38, 227, 118, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_2_1);
+		
+		textField_14 = new JTextField();
+		textField_14.setColumns(10);
+		textField_14.setBounds(172, 224, 156, 20);
+		RegistrarProveedor.getContentPane().add(textField_14);
+		
+		JLabel lblNewLabel_1_1_1_3_1 = new JLabel("Link Web");
+		lblNewLabel_1_1_1_3_1.setBounds(38, 272, 48, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_1_3_1);
+		
+		textField_15 = new JTextField();
+		textField_15.setColumns(10);
+		textField_15.setBounds(96, 269, 232, 20);
+		RegistrarProveedor.getContentPane().add(textField_15);
+		
+		textField_11 = new JTextField();
+		textField_11.setColumns(10);
+		textField_11.setBounds(38, 337, 37, 20);
+		RegistrarProveedor.getContentPane().add(textField_11);
+		
+		JLabel lblNewLabel_1_2_1_2 = new JLabel("Fecha de Nacimiento");
+		lblNewLabel_1_2_1_2.setBounds(38, 312, 102, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_2_1_2);
+		
+		JLabel lblNewLabel_1_1_1_2_2 = new JLabel("Día");
+		lblNewLabel_1_1_1_2_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1_2_2.setBounds(38, 360, 37, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_1_2_2);
+		
+		JLabel lblNewLabel_1_1_1_1_2 = new JLabel("/");
+		lblNewLabel_1_1_1_1_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1_1_1_1_2.setBounds(85, 339, 5, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_1_1_2);
+		
+		textField_12 = new JTextField();
+		textField_12.setColumns(10);
+		textField_12.setBounds(100, 337, 37, 20);
+		RegistrarProveedor.getContentPane().add(textField_12);
+		
+		JLabel lblNewLabel_1_1_1_2_1_2 = new JLabel("Mes");
+		lblNewLabel_1_1_1_2_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1_2_1_2.setBounds(100, 360, 37, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_1_2_1_2);
+		
+		JLabel lblNewLabel_1_1_1_1_1_1 = new JLabel("/");
+		lblNewLabel_1_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1_1_1_1_1_1.setBounds(147, 339, 5, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_1_1_1_1);
+		
+		textField_13 = new JTextField();
+		textField_13.setColumns(10);
+		textField_13.setBounds(162, 337, 37, 20);
+		RegistrarProveedor.getContentPane().add(textField_13);
+		
+		JLabel lblNewLabel_1_1_1_2_1_1_1 = new JLabel("Año");
+		lblNewLabel_1_1_1_2_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1_1_2_1_1_1.setBounds(162, 360, 37, 14);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_1_1_1_2_1_1_1);
+		
+		JButton lblNewLabel_2_1 = new JButton("Asignar una imagen");
+		lblNewLabel_2_1.setBounds(38, 398, 144, 20);
+		RegistrarProveedor.getContentPane().add(lblNewLabel_2_1);
+		RegistrarProveedor.setVisible(true);
+		RegistrarCliente.setVisible(true);
 	}
-
 }
