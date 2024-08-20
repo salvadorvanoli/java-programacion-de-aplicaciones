a822dcf3caf0b8319416e63ad263df080e328874
Blckdev <23felipecamejo@gmail.com>
2024-08-19 22:22:38 -0300

    Inicie una de mis interfaces, ademas mejoré el diseño de la interfaz general.

--- java_programacion_de_aplicaciones/src/interfaces/Principal.java
+++ java_programacion_de_aplicaciones/src/interfaces/Principal.java
@@ -11,11 +11,28 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import javax.swing.JInternalFrame;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import javax.swing.JTextArea;
+import javax.swing.JFormattedTextField;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Principal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -37,29 +54,32 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\felip\\Downloads\\Diseño sin título (1).png"));
+		setBackground(new Color(255, 255, 255));
+		setTitle("FlaminGo");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 647, 695);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnNewMenu = new JMenu("Sesión");
-		menuBar.add(mnNewMenu);
+		JMenu MenuSesión = new JMenu("Sesión");
+		menuBar.add(MenuSesión);
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("Cliente");
-		mnNewMenu.add(mntmNewMenuItem);
+		MenuSesión.add(mntmNewMenuItem);
 		
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Proveedor");
-		mnNewMenu.add(mntmNewMenuItem_1);
+		MenuSesión.add(mntmNewMenuItem_1);
 		
 		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Administrador");
-		mnNewMenu.add(mntmNewMenuItem_2);
+		MenuSesión.add(mntmNewMenuItem_2);
 		
-		JMenu mnNewMenu_1 = new JMenu("Casos de usos");
-		menuBar.add(mnNewMenu_1);
+		JMenu MenuCasosDeUsos = new JMenu("Casos de usos");
+		menuBar.add(MenuCasosDeUsos);
 		
 		JMenu mnNewMenu_2 = new JMenu("Registros");
-		mnNewMenu_1.add(mnNewMenu_2);
+		MenuCasosDeUsos.add(mnNewMenu_2);
 		
 		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Usuario");
 		mnNewMenu_2.add(mntmNewMenuItem_3);
@@ -74,7 +94,7 @@ public class Principal extends JFrame {
 		mnNewMenu_2.add(mntmNewMenuItem_12);
 		
 		JMenu mnNewMenu_3 = new JMenu("Consultas");
-		mnNewMenu_1.add(mnNewMenu_3);
+		MenuCasosDeUsos.add(mnNewMenu_3);
 		
 		JMenuItem mntmNewMenuItem_6 = new JMenuItem("Información cliente");
 		mnNewMenu_3.add(mntmNewMenuItem_6);
@@ -89,20 +109,74 @@ public class Principal extends JFrame {
 		mnNewMenu_3.add(mntmNewMenuItem_9);
 		
 		JMenu mnNewMenu_4 = new JMenu("Modificaciones");
-		mnNewMenu_1.add(mnNewMenu_4);
+		MenuCasosDeUsos.add(mnNewMenu_4);
 		
 		JMenuItem mntmNewMenuItem_10 = new JMenuItem("Producto");
 		mnNewMenu_4.add(mntmNewMenuItem_10);
 		
 		JMenu mnNewMenu_5 = new JMenu("Supresiones");
-		mnNewMenu_1.add(mnNewMenu_5);
+		MenuCasosDeUsos.add(mnNewMenu_5);
 		
 		JMenuItem mntmNewMenuItem_11 = new JMenuItem("Orden de Compra");
 		mnNewMenu_5.add(mntmNewMenuItem_11);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 153, 204));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JInternalFrame RegistrarProducto = new JInternalFrame("Registrar Producto");
+		RegistrarProducto.setBounds(207, 28, 235, 205);
+		RegistrarProducto.setPreferredSize(new Dimension(250, 300));
+		RegistrarProducto.setFrameIcon(new ImageIcon("C:\\Users\\felip\\Downloads\\Diseño sin título (1).png"));
+		RegistrarProducto.setClosable(true);
+		RegistrarProducto.setResizable(true);
+		contentPane.add(RegistrarProducto);
+		RegistrarProducto.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Descripción");
+		lblNewLabel.setBounds(10, 55, 54, 14);
+		RegistrarProducto.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 25, 86, 20);
+		RegistrarProducto.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(110, 25, 86, 20);
+		RegistrarProducto.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre");
+		lblNewLabel_1.setBounds(10, 11, 46, 14);
+		RegistrarProducto.getContentPane().add(lblNewLabel_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 69, 186, 43);
+		RegistrarProducto.getContentPane().add(textArea);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Especificación");
+		lblNewLabel_1_1.setBounds(110, 11, 71, 14);
+		RegistrarProducto.getContentPane().add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Precio");
+		lblNewLabel_1_2.setBounds(10, 125, 29, 14);
+		RegistrarProducto.getContentPane().add(lblNewLabel_1_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(10, 139, 71, 20);
+		RegistrarProducto.getContentPane().add(textField_2);
+		
+		JLabel lblNewLabel_1_2_1 = new JLabel("Imagen");
+		lblNewLabel_1_2_1.setBounds(110, 125, 46, 14);
+		RegistrarProducto.getContentPane().add(lblNewLabel_1_2_1);
+		
+		JButton btnNewButton = new JButton("Seleccionar");
+		btnNewButton.setBounds(107, 138, 89, 23);
+		RegistrarProducto.getContentPane().add(btnNewButton);
+		RegistrarProducto.setVisible(true);
 	}
-
 }
