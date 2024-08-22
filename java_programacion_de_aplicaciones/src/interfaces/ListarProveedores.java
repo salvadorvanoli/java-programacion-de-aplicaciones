a08149f2a162a6972c1763fda5ec332428082457
Blckdev <23felipecamejo@gmail.com>
2024-08-22 11:23:09 -0300

    Mejora de las Interface

--- java_programacion_de_aplicaciones/src/interfaces/ListarProveedores.java
+++ java_programacion_de_aplicaciones/src/interfaces/ListarProveedores.java
@@ -35,35 +35,36 @@ public class ListarProveedores extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public ListarProveedores() {
-		setTitle("Elija un Proveedor");
+		setTitle("Flamin-Go");
 		setFrameIcon(new ImageIcon("C:\\Users\\felip\\Downloads\\Diseño sin título (1).png"));
 		getContentPane().setBackground(new Color(255, 192, 203));
-		setBounds(100, 100, 142, 160);
+		setBounds(100, 100, 186, 160);
 		getContentPane().setLayout(null);
 		
-		JLabel TextoProveedor = new JLabel("Eleccion:");
+		JLabel TextoProveedor = new JLabel("Elija un Proveedor\r\n");
+		TextoProveedor.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		TextoProveedor.setHorizontalAlignment(SwingConstants.CENTER);
-		TextoProveedor.setBounds(16, 11, 91, 14);
+		TextoProveedor.setBounds(0, 11, 178, 14);
 		getContentPane().add(TextoProveedor);
 		
 		JComboBox<Object> comboBox = new JComboBox<Object>();
-		comboBox.setBounds(16, 25, 91, 14);
+		comboBox.setBounds(41, 34, 91, 14);
 		getContentPane().add(comboBox);
 		
 		JLabel Nick_1 = new JLabel("Nick");
 		Nick_1.setHorizontalAlignment(SwingConstants.CENTER);
 		Nick_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		Nick_1.setBounds(0, 45, 127, 28);
+		Nick_1.setBounds(0, 47, 178, 28);
 		getContentPane().add(Nick_1);
 		
 		JLabel Email_2 = new JLabel("Email");
 		Email_2.setHorizontalAlignment(SwingConstants.CENTER);
 		Email_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		Email_2.setBounds(0, 72, 127, 28);
+		Email_2.setBounds(0, 72, 178, 28);
 		getContentPane().add(Email_2);
 		
 		JButton BotonConfirmar = new JButton("Confirmar");
-		BotonConfirmar.setBounds(16, 107, 91, 23);
+		BotonConfirmar.setBounds(41, 107, 91, 23);
 		getContentPane().add(BotonConfirmar);
 
 	}
