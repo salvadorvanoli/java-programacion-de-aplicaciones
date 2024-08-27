84f6c99a25019f83fd560f02838bbbc918b88e0e
jgmva <jgmva@Casa_PC>
2024-08-27 20:17:56 -0300

    cambie nombre var clientes

--- java_programacion_de_aplicaciones/src/interfaces/RegistrarCliente.java
+++ java_programacion_de_aplicaciones/src/interfaces/RegistrarCliente.java
@@ -14,14 +14,16 @@ import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.ImageIcon;
 import com.toedter.calendar.JDateChooser;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegistrarCliente extends JInternalFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField textNick;
+	private JTextField textMail;
+	private JTextField textNom;
+	private JTextField textApe;
 
 	/**
 	 * Launch the application.
@@ -52,64 +54,69 @@ public class RegistrarCliente extends JInternalFrame {
 		setBounds(100, 100, 385, 405);
 		getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Registrar Cliente");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(0, 11, 369, 29);
-		getContentPane().add(lblNewLabel);
+		JLabel LabelRegCli = new JLabel("Registrar Cliente");
+		LabelRegCli.setHorizontalAlignment(SwingConstants.CENTER);
+		LabelRegCli.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		LabelRegCli.setBounds(0, 11, 369, 29);
+		getContentPane().add(LabelRegCli);
 		
-		JLabel lblNewLabel_1 = new JLabel("Nickname");
-		lblNewLabel_1.setBounds(38, 54, 48, 14);
-		getContentPane().add(lblNewLabel_1);
+		JLabel LabelNick = new JLabel("Nickname");
+		LabelNick.setBounds(38, 54, 48, 14);
+		getContentPane().add(LabelNick);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(106, 51, 222, 20);
-		getContentPane().add(textField);
+		textNick = new JTextField();
+		textNick.setColumns(10);
+		textNick.setBounds(106, 51, 222, 20);
+		getContentPane().add(textNick);
 		
-		JLabel lblNewLabel_1_2 = new JLabel("Correo Electrónico");
-		lblNewLabel_1_2.setBounds(38, 98, 89, 14);
-		getContentPane().add(lblNewLabel_1_2);
+		JLabel LabelMail = new JLabel("Correo Electrónico");
+		LabelMail.setBounds(38, 98, 89, 14);
+		getContentPane().add(LabelMail);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(148, 95, 180, 20);
-		getContentPane().add(textField_1);
+		textMail = new JTextField();
+		textMail.setColumns(10);
+		textMail.setBounds(148, 95, 180, 20);
+		getContentPane().add(textMail);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Nombre");
-		lblNewLabel_1_1.setBounds(38, 143, 48, 14);
-		getContentPane().add(lblNewLabel_1_1);
+		JLabel LabelNom = new JLabel("Nombre");
+		LabelNom.setBounds(38, 143, 48, 14);
+		getContentPane().add(LabelNom);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(96, 140, 232, 20);
-		getContentPane().add(textField_2);
+		textNom = new JTextField();
+		textNom.setColumns(10);
+		textNom.setBounds(96, 140, 232, 20);
+		getContentPane().add(textNom);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Apellido");
-		lblNewLabel_1_1_1.setBounds(38, 188, 48, 14);
-		getContentPane().add(lblNewLabel_1_1_1);
+		JLabel LabelApe = new JLabel("Apellido");
+		LabelApe.setBounds(38, 188, 48, 14);
+		getContentPane().add(LabelApe);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(96, 185, 232, 20);
-		getContentPane().add(textField_3);
+		textApe = new JTextField();
+		textApe.setColumns(10);
+		textApe.setBounds(96, 185, 232, 20);
+		getContentPane().add(textApe);
 		
-		JLabel lblNewLabel_1_2_1 = new JLabel("Fecha de Nacimiento");
-		lblNewLabel_1_2_1.setBounds(38, 233, 102, 14);
-		getContentPane().add(lblNewLabel_1_2_1);
+		JLabel LabelFecha = new JLabel("Fecha de Nacimiento");
+		LabelFecha.setBounds(38, 233, 102, 14);
+		getContentPane().add(LabelFecha);
 		
-		JButton lblNewLabel_2 = new JButton("Asignar una imagen");
-		lblNewLabel_2.setBounds(38, 319, 144, 20);
-		getContentPane().add(lblNewLabel_2);
+		JButton ButtonImg = new JButton("Asignar una imagen");
+		ButtonImg.setBounds(38, 319, 144, 20);
+		getContentPane().add(ButtonImg);
 		
-		JButton btnNewButton = new JButton("Registrar");
-		btnNewButton.setBackground(new Color(250, 214, 235));
-		btnNewButton.setBounds(255, 335, 89, 23);
-		getContentPane().add(btnNewButton);
+		JButton ButtonReg = new JButton("Registrar");
+		ButtonReg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String nickname = textNicknameCliente.getText();
+			}
+		});
+		ButtonReg.setBackground(new Color(250, 214, 235));
+		ButtonReg.setBounds(255, 335, 89, 23);
+		getContentPane().add(ButtonReg);
 		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(38, 258, 144, 20);
-		getContentPane().add(dateChooser);
+		JDateChooser DateFecha = new JDateChooser();
+		DateFecha.setBounds(38, 258, 144, 20);
+		getContentPane().add(DateFecha);
 
 	}
 }
